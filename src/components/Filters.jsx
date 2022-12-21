17e57935add7e30c8be459cdfd7cae12baf2b3b8
Gabriel Ramires <falivith@gmail.com>
2022-12-21 15:01:14 -0300

    Art

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -11,25 +11,27 @@ export function Filters() {
 
             <div className = { styles.selectorExternalContainer }>
                 <span className = { styles.blueSpan }>Área do Conhecimento</span>
-                <div className = { styles.internalContainerMenu }>
-                    <select className = { styles.selectMenu }>
+                <div className = { styles.lsCustomSelect }>
+                    <select className = { styles.lsSelect } name = "area-conhecimento">
                         <option value="0">Todas</option>
                         <option value="1">Português</option>
                         <option value="2">Matemática</option>
                     </select>
+                    <span className = { styles.customArrow }></span>
                 </div>
             </div>
 
             <div className = { styles.selectorExternalContainer }>
                 <span className = { styles.blueSpan }>Tipo do Material</span>
-                <div className = { styles.internalContainerMenu }>
-                    <select className = { styles.selectMenu }>
+                <div className = { styles.lsCustomSelect }>
+                    <select className = { styles.lsSelect } name = "tipo-material">
                         <option value="0">Todos</option>
                         <option value="1">Vídeo</option>
                         <option value="2">Site</option>
                         <option value="3">Artigo</option>
                         <option value="4">Documento</option>
                     </select>
+                    <span className = { styles.customArrow }></span>
                 </div>  
             </div>
 
