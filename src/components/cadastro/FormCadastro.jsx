5996f6329bdf69e1f79534278c381afeef06dbb9
kevin-weitgenant <kevin.weitgenant@gmail.com>
2023-02-16 00:50:08 -0300

    cadastro progresso

--- src/components/cadastro/FormCadastro.jsx
+++ src/components/cadastro/FormCadastro.jsx
@@ -1,6 +1,7 @@
 import styles from './FormCadastro.module.css';
 import { LabelAndInput } from '../login/LabelAndInput';
-
+import { Button } from '../login/Button';
+import styled from "styled-components";
 
 export function FormCadastro() {
 
@@ -12,8 +13,12 @@ return(
             <div className={styles.divSpacing}> <LabelAndInput labelText={'INSTITUIÇÃO DE ENSINO'} inputType={'text'} placeholderText={'Instituição de ensino'}/>          </div>
             <div className={styles.divSpacing}> <LabelAndInput labelText={'PERFIL'} inputType={'text'} placeholderText={'Selecione...'}/>          </div>
             <div className={styles.divSpacing}> <LabelAndInput labelText={'E-MAIL'} inputType={'email'} placeholderText={'E-mail'}/>          </div>
-
-        </form>
+            <div className={styles.twoItens}> 
+            <LabelAndInput labelText={'SENHA'} inputType={'password'} placeholderText={'• • • • • • •'}/>          
+            <LabelAndInput labelText={'REPETIR SENHA'} inputType={'password'} placeholderText={'• • • • • • •'}/>        
+            </div>
+            <div style= {{"margin-top": "1rem"}}> <Button textButton={"CADASTRAR"}/> </div>
+        </form> ''
     </div>
 )
 
