c9cf88b2755b36b8b23a9dc4cbd446345001883d
Gabriel Ramires <falivith@gmail.com>
2023-03-07 22:32:36 -0300

    PluginReas Screen Done

--- src/components/addrea/PluginReasPanel.jsx
+++ src/components/addrea/PluginReasPanel.jsx
@@ -2,6 +2,21 @@ import styles from './PluginReasPanel.module.css'
 import AddRing from "../../assets/Add_ring_green.png";
 import { PluginResourceContainer } from "./PluginResourceContainer"
 
+const reas = [
+    {
+        id: 1,
+        title: "Histórico dos Recursos Educacionais Abertos no Brasil e no Mundo",
+    },
+    {
+        id: 2,
+        title: "Escolas, universidades, ONGs, governos, etc.",
+    },
+    {
+        id: 3,
+        title: "Licenças de direito autoral e Creative Commons, formatos abertos e formatos fechados",
+    }
+  ];
+
 export function PluginReasPanel(){
     return(
         <div className = { styles.container }>
@@ -13,7 +28,13 @@ export function PluginReasPanel(){
                 <span>Você tem <span className = { styles.pluginReaCounter }>3</span> recursos salvos que ainda não foram adicionados.</span>
             </div>
             <div className = { styles.pluginRequester }>
-                Histórico dos Recursos Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis, hic. Inventore atque incidunt, excepturi unde tempora cum soluta eius magni a repellat nemo quas non ad harum aperiam laborum iste.
+                {reas.map(rea => {
+                    return <PluginResourceContainer
+                        key = {rea.id}
+                        title = {rea.title}
+                    />
+                    })
+                }
             </div>
             <footer>
                 <p className = { styles.footerSpan }>Além do plugin, você pode adicionar um recurso próprio que está armazenado online (ex: Google Drive, Dropbox, etc)</p>
--- src/components/addrea/PluginResourceContainer.jsx
+++ src/components/addrea/PluginResourceContainer.jsx
@@ -3,41 +3,8 @@ import styles from "./PluginResourceContainer.module.css";
 export function PluginResourceContainer(props){
     return(
         <div className = { styles.container }>
-            <h1>
-               { props.title }
-            </h1>
-            <button>
-                Adicionar
-            </button>
-        </div>
-    )
-}
-
-const reas = [
-    {
-        id: 1,
-        title: "Histórico dos Recursos Educacionais Abertos no Brasil e no Mundo",
-    },
-    {
-        id: 2,
-        title: "Escolas, universidades, ONGs, governos, etc.",
-    },
-    {
-        id: 3,
-        title: "Licenças de direito autoral e Creative Commons, formatos abertos e formatos fechados",
-    }
-  ];
-
-export function ReaList() {
-    return (
-        <div className = { styles.reaContainer }>
-            {reas.map(rea => {
-                return <ReaPreview
-                    key = {rea.id}
-                    title = {rea.title}
-                />
-                })
-            }
+            <h1>{ props.title }</h1>
+            <button>ADICIONAR</button>
         </div>
     )
 }
