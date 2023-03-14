7b4ee43f75c95370e2f49555c2cd6e1aab843ba7
kevin-weitgenant <kevin.weitgenant@gmail.com>
2023-03-14 11:40:15 -0300

    fix axios post

--- src/services/authentication.js
+++ src/services/authentication.js
@@ -7,7 +7,7 @@ const login = async credentials => {
 }
 
 const register = async credentials => {
-  const response = await axios.post('/api/registration', credentials)
+  const response = await axios.post('/api/users', credentials)
   return response.data
 }
 
