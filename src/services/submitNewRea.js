752c2d2cedb9173b51bd37d2c567107597e90a87
kevin-weitgenant <kevin.weitgenant@gmail.com>
2023-03-22 14:56:52 -0300

    Update submitNewRea.js

--- src/services/submitNewRea.js
+++ src/services/submitNewRea.js
@@ -1,6 +1,17 @@
 import axios from 'axios'
+import { checkLoginStatus } from './utils';
 
 const baseUrl = axios.create({
   baseURL: 'http://localhost:3001'
 });
 
+export async function submitRea(recurso){
+  user,config = checkLoginStatus()
+
+  if(user){
+    const response = await baseUrl.post(`/api/users/${userObject.email}`, recurso, config)
+  }
+  else{
+    console.log("Sem usuário logado");
+  }
+}
