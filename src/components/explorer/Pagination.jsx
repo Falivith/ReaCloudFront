92b60129683f5c7359cc19eb220018c165224383
kevin-weitgenant <kevin.weitgenant@gmail.com>
2023-08-22 11:08:45 -0300

    pagination

--- src/components/explorer/Pagination.jsx
+++ src/components/explorer/Pagination.jsx
@@ -2,17 +2,33 @@ import styles from './Pagination.module.css';
 import Preview from '../../assets/LeftArrow.svg';
 import Next from '../../assets/RightArrow.svg';
 
-export function Pagination(){
-    return(
-        <div className = { styles.pagination }>
-            <a href="#"><img src = { Preview } alt="preview" /></a>
-            <a href="#">1</a>
-            <a href="#" className = { styles.active }>2</a>
-            <a href="#">3</a>
-            <a href="#">4</a>
-            <a href="#">5</a>
-            <a href="#">6</a>
-            <a href="#"><img src = { Next } alt="next" /></a>
+export function Pagination({ setCurrentPage,currentPage }) {
+    const handlePageClick = (event) => {
+        event.preventDefault();
+        const newPage = parseInt(event.target.innerHTML);
+        setCurrentPage(newPage);
+    };
+
+    const handleNextClick = (event) => {
+        console.log("currentPage = ",currentPage)
+        event.preventDefault();
+        setCurrentPage((prevPage) => prevPage + 1);
+    };
+
+    return (
+        <div className={styles.pagination}>
+            <a href="#"><img src={Preview} alt="preview" /></a>
+            {[1, 2, 3, 4, 5, 6].map((page) => (
+                <a 
+                    href="#" 
+                    key={page} 
+                    onClick={handlePageClick} 
+                    className={page === currentPage ? styles.active : ''}
+                >
+                    {page}
+                </a>
+            ))}
+            <a href="#" onClick={handleNextClick}><img src={Next} alt="next" /></a>
         </div>
-    )
+    );
 }
