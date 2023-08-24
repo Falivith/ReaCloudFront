c15467d9284a574d1bb0fcc1356b86f22e366a50
kevin-weitgenant <kevin.weitgenant@gmail.com>
2023-08-24 10:31:10 -0300

    states pra pagina do recurso

--- src/components/reaview/ReaPanel.jsx
+++ src/components/reaview/ReaPanel.jsx
@@ -2,8 +2,17 @@ import styles from './ReaPanel.module.css';
 import Like from '../../assets/Like.png';
 import Comments from '../../assets/Comments.png';
 import ReaThumb from '../../assets/ExampleRea.png';
-
+import { useState } from 'react';
 export function ReaPanel(){
+
+    const [tipoMaterial, setTipoMaterial] = useState('Website');
+    const [areaConhecimento, setAreaConhecimento] = useState('Matemática');
+    const [tipoLicenca, setTipoLicenca] = useState('Creative Commons');
+    const [publicoAlvo, setPublicoAlvo] = useState('Ensino Fundamental');
+    const [idioma, setIdioma] = useState('Português');
+    const [descricao, setDescricao] = useState('Lorem, ipsum dolor sit amet consectetur adipisicing elit...');
+    const [instrucoesUso, setInstrucoesUso] = useState('Lorem ipsum dolor sit amet consectetur, adipisicing elit...');
+
     return (
         <div className = { styles.container }>
             <div className = { styles.thumbAuxContainer }>
@@ -19,15 +28,29 @@ export function ReaPanel(){
                 <img src = { ReaThumb } alt="reaThumb" />  
             </div>
            
-            <ul className = { styles.metaData } >
-                <li> <strong>Tipo do Material:</strong> Website </li>
-                <li> <strong>Área do conhecimento:</strong> Matemática </li>
-                <li> <strong>Tipo de Licença:</strong> Creative Commons </li>
-                <li> <strong>Público alvo:</strong> Ensino Fundamental</li>
-                <li> <strong>Idioma:</strong> Português </li>
-                <li> <strong>Descrição:</strong> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto praesentium, quis a temporibus quam molestiae eum laborum odio officia ipsa animi cum, consequuntur culpa eaque delectus! Dolore sint illum libero!</li>
-                <li> <strong>Instruções de uso:</strong> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore consequatur eaque dolores quod sit alias suscipit odio ducimus libero perspiciatis quasi cum fuga eveniet, at dicta deleniti voluptatibus repellat obcaecati?</li>
-            </ul>
+            <ul className={styles.metaData}>
+            <li>
+                <strong>Tipo do Material:</strong> {tipoMaterial}
+            </li>
+            <li>
+                <strong>Área do conhecimento:</strong> {areaConhecimento}
+            </li>
+            <li>
+                <strong>Tipo de Licença:</strong> {tipoLicenca}
+            </li>
+            <li>
+                <strong>Público alvo:</strong> {publicoAlvo}
+            </li>
+            <li>
+                <strong>Idioma:</strong> {idioma}
+            </li>
+            <li>
+                <strong>Descrição:</strong> {descricao}
+            </li>
+            <li>
+                <strong>Instruções de uso:</strong> {instrucoesUso}
+            </li>
+        </ul>
         </div>
     );
 }
