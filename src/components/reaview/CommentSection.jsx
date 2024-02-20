3167596e33a8d07f9e272068fca9e99a0c810c39
Maria Eduarda Carmona <eduardacarmona@gmail.com>
2024-02-20 12:09:35 -0300

    teste comentários

--- src/components/reaview/CommentSection.jsx
+++ src/components/reaview/CommentSection.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from './CommentSection.module.css';
 import { Comment } from './Comment';
 import avatar1 from './Avatar.webp';
 import avatar2 from './Avatar2.png';
 import { submitComment } from '../../services/comment';
+import { getCommentInfo } from '../../services/comment';
 
 const mocados = [
     {
@@ -20,21 +21,39 @@ const mocados = [
     },
 ]
 
-export function CommentSection({resourceId}){
-
-        const [commentText, setCommentText] = useState('');
-        
-        const handleCommentChange = (event) => {
-            setCommentText(event.target.value);
-        };
-        
-        const handleSubmitComment = (event) => {
-            event.preventDefault();
-    
-            submitComment(commentText,  resourceId);
-            console.log('Comentário enviado:', commentText);
-            setCommentText('');
-        };
+export function CommentSection({ resourceId }) {
+    const [commentText, setCommentText] = useState('');
+    const [comments, setComments] = useState([]);
+  
+    useEffect(() => {
+      // Fetch comments when the component mounts or when resourceId changes
+      fetchComments();
+    }, [resourceId]);
+  
+    const fetchComments = async () => {
+      try {
+        const commentInfo = await getCommentInfo(resourceId);
+        setComments(commentInfo);
+      } catch (error) {
+        console.error('Error fetching comments:', error);
+      }
+    };
+  
+    const handleCommentChange = (event) => {
+      setCommentText(event.target.value);
+    };
+  
+    const handleSubmitComment = async (event) => {
+      event.preventDefault();
+  
+      await submitComment(commentText, resourceId);
+      console.log('Comentário enviado:', commentText);
+  
+      // Fetch comments after submitting a new comment
+      fetchComments();
+  
+      setCommentText('');
+    };
 
     return(
         <div className = { styles.container }>
@@ -58,8 +77,8 @@ export function CommentSection({resourceId}){
             </form>
 
             <div className = { styles.commentList }>
-                {mocados.map((comment, index) => (
-                    <Comment key={index} nome={comment.nome} text={comment.text} date={comment.date} foto={comment.foto} />
+                {comments.map( (comment, index) => (
+                    <Comment key={index} nome={comment.user_id} text={comment.comment} date={comment.createdAt} foto={comment.foto} />
                 ))}
             </div>
 
