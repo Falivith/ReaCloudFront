e6bcf5c4891e3370143ee8b244bcff7b21dc17e8
Maria Eduarda Carmona <eduardacarmona@gmail.com>
2025-02-20 17:38:20 -0300

    Help page template

---
+++ src/components/help-page/common-issues.jsx
@@ -0,0 +1,137 @@
+import styles from "./faq.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleRight } from "@fortawesome/free-solid-svg-icons";
+import {  useState } from "react";
+
+export function CommonIssues() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleContent = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div
+        className={styles.titleContainer}
+        onClick={toggleContent}
+        style={{ cursor: "pointer" }}
+      >
+        <h1 className={styles.reaTitle}>
+          <FontAwesomeIcon
+            icon={faCircleRight}
+            style={{ marginRight: "5px", color: "var(--green)" }}
+          />
+          Problemas Comuns
+        </h1>
+      </div>
+      <div className={`${styles.FaqContent} ${isOpen ? styles.open : ''}`}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          quam architecto ipsum autem recusandae optio, illo minima! Odit qui
+          quidem iusto? Id, et alias possimus quasi nihil sint vitae doloribus!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          quam architecto ipsum autem recusandae optio, illo minima! Odit qui
+          quidem iusto? Id, et alias possimus quasi nihil sint vitae doloribus!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          quam architecto ipsum autem recusandae optio, illo minima! Odit qui
+          quidem iusto? Id, et alias possimus quasi nihil sint vitae doloribus!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          quam architecto ipsum autem recusandae optio, illo minima! Odit qui
+          quidem iusto? Id, et alias possimus quasi nihil sint vitae doloribus!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          quam architecto ipsum autem recusandae optio, illo minima! Odit qui
+          quidem iusto? Id, et alias possimus quasi nihil sint vitae doloribus!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          quam architecto ipsum autem recusandae optio, illo minima! Odit qui
+          quidem iusto? Id, et alias possimus quasi nihil sint vitae doloribus!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          quam architecto ipsum autem recusandae optio, illo minima! Odit qui
+          quidem iusto? Id, et alias possimus quasi nihil sint vitae doloribus!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          quam architecto ipsum autem recusandae optio, illo minima! Odit qui
+          quidem iusto? Id, et alias possimus quasi nihil sint vitae doloribus!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          quam architecto ipsum autem recusandae optio, illo minima! Odit qui
+          quidem iusto? Id, et alias possimus quasi nihil sint vitae doloribus!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          quam architecto ipsum autem recusandae optio, illo minima! Odit qui
+          quidem iusto? Id, et alias possimus quasi nihil sint vitae doloribus!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          quam architecto ipsum autem recusandae optio, illo minima! Odit qui
+          quidem iusto? Id, et alias possimus quasi nihil sint vitae doloribus!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          quam architecto ipsum autem recusandae optio, illo minima! Odit qui
+          quidem iusto? Id, et alias possimus quasi nihil sint vitae doloribus!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          quam architecto ipsum autem recusandae optio, illo minima! Odit qui
+          quidem iusto? Id, et alias possimus quasi nihil sint vitae doloribus!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          quam architecto ipsum autem recusandae optio, illo minima! Odit qui
+          quidem iusto? Id, et alias possimus quasi nihil sint vitae doloribus!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          quam architecto ipsum autem recusandae optio, illo minima! Odit qui
+          quidem iusto? Id, et alias possimus quasi nihil sint vitae doloribus!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          quam architecto ipsum autem recusandae optio, illo minima! Odit qui
+          quidem iusto? Id, et alias possimus quasi nihil sint vitae doloribus!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          quam architecto ipsum autem recusandae optio, illo minima! Odit qui
+          quidem iusto? Id, et alias possimus quasi nihil sint vitae doloribus!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          quam architecto ipsum autem recusandae optio, illo minima! Odit qui
+          quidem iusto? Id, et alias possimus quasi nihil sint vitae doloribus!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          quam architecto ipsum autem recusandae optio, illo minima! Odit qui
+          quidem iusto? Id, et alias possimus quasi nihil sint vitae doloribus!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          quam architecto ipsum autem recusandae optio, illo minima! Odit qui
+          quidem iusto? Id, et alias possimus quasi nihil sint vitae doloribus!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          quam architecto ipsum autem recusandae optio, illo minima! Odit qui
+          quidem iusto? Id, et alias possimus quasi nihil sint vitae doloribus!
+        </p>
+      </div>
+    </div>
+  );
+}
